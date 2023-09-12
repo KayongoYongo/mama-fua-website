b6db8ac2877ad391f6bb96646d264d9e30424e12
KayongoYongo <sknsammie10@gmail.com>
2023-09-12 19:22:54 +0300

    sign_up

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Basic flask app
+"""
+from os import getenv
+from views.auth.auth import Auth
+from flask import Flask, jsonify, request, abort, redirect
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def index() -> str:
+    """Returns a simple JSONIFY request
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """A function that impiments a POST method on the users table.
+
+    Args:
+        None
+
+    Return:
+        Return: A jsonify message
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # register user if user does not exist
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ views/auth/auth.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""A function that encrypts a password
+"""
+from models.user import User
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+import bcrypt
+from uuid import uuid4
+from db import DB
+
+
+def _hash_password(password: str) -> str:
+    """
+    A method for hashing out a password
+
+    Args:
+        Password: The string to be hashed out
+
+    Return:
+        The password in byte format
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+def _generate_uuid() -> str:
+    """Generate uuid
+    Args:
+        None
+
+    Return:
+        string
+    """
+    return str(uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """A method that initializes the DB object
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> Union[None, User]:
+        """
+        A method for registering users.
+
+        Args:
+            email: The email of the user
+            password: The password of the user
+
+        Return:
+            None or user object
+        """
+        try:
+            # find the user with the given email
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError('User {} already exists'.format(email))
